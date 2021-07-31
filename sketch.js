83e2371050491c02c76addd42ec5819002e7ce8a
adamgeojose <87291325+adamgeojose@users.noreply.github.com>
2021-07-31 20:41:22 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,18 @@
+var ship
+var sea1
+var ship ,sea1;
+
+function preload(){
+ship=loadAnimation("ship-1.png , ship-2.png , ship-3.pngship-4.png")
+sea1=loadAnimation("sea.png")
+}
+
+function setup(){
+  createCanvas(400,400);
+  
+}
+
+function draw() {
+  background("blue");
+ 
+}
